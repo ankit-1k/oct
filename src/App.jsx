c313149c9adc3f81bba40f42684525534f1ba4f8
Ankit Kumar Panda <105581787+ankit-1k@users.noreply.github.com>
2024-03-31 22:43:44 +0530

    Updated the app UI and fixed a bug in the authentication process.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-// import Setinterval from './components/Setinterval'
 import './App.css'
-// import ExpVoice from './Speek/ExpVoice'
-// import Context from './components/Context'
-import useHook from './components/useHook'
-import Child from './components/Child'
 const App = () => {
   return (
     <div className='bg'>
-      app
+      appin prosjdflsj
     </div>
   )
 }
